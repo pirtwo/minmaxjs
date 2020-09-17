7c3879d20b37440a512ed12af416710aa17fde6e
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2020-09-17 12:26:06 +0430

    update test suite

--- test/minmax.test.js
+++ test/minmax.test.js
@@ -1,83 +1,48 @@
-const Node = require('../src/node');
+const MockNode = require('../src/mock-node');
 const minmax = require('../src/minmax');
-
+const alphabeta = require('../src/alphabeta');
 
 test('test minmax', () => {
-    expect(minmax(new MyNode({
+    expect(minmax(new MockNode({
         type: 'max',
         state: "0000",
     }))).toBe(0);
 
-    expect(minmax(new MyNode({
+    expect(minmax(new MockNode({
         type: 'min',
         state: "0000",
     }))).toBe(0);
 
-    expect(minmax(new MyNode({
+    expect(minmax(new MockNode({
         type: 'max',
         state: "xo00",
     }))).toBe(1);
 
-    expect(minmax(new MyNode({
+    expect(minmax(new MockNode({
         type: 'min',
         state: "ox00",
     }))).toBe(-1);
-})
+});
 
+test('test alpha-beta pruning', ()=>{
+    expect(alphabeta(new MockNode({
+        type: 'max',
+        state: "0000",
+    })).value).toBe(0);
 
-// creating custom scenario
-// 
-// max   -> x 
-// min   -> o
-// state -> "0000"
-// x win -> {"x0ox", "xo0x", "0xxo", "oxx0"}
-// o win -> {"o0xo", "ox0o", "0oox", "xoo0"}
+    expect(alphabeta(new MockNode({
+        type: 'min',
+        state: "0000",
+    })).value).toBe(0);
 
-const xwinStates = ["x0ox", "xo0x", "0xxo", "oxx0"];
-const owinStates = ["o0xo", "ox0o", "0oox", "xoo0"];
+    expect(alphabeta(new MockNode({
+        type: 'max',
+        state: "xo00",
+    })).value).toBe(1);
 
-class MyNode extends Node {
-    constructor({
-        type,
-        state,
-        parent
-    }) {
-        super({
-            type,
-            state,
-            parent
-        });
-    }
+    expect(alphabeta(new MockNode({
+        type: 'min',
+        state: "ox00",
+    })).value).toBe(-1);
+});
 
-    calcValue() {
-        if (owinStates.indexOf(this.state) > -1)
-            return -1;
-        if (xwinStates.indexOf(this.state) > -1)
-            return 1;
-        return 0;
-    }
-
-    isTerminal() {
-        return xwinStates.indexOf(this.state) > -1 ||
-            owinStates.indexOf(this.state) > -1 || !this.state.includes('0');
-    }
-
-    getChilds() {
-        let childs = [],
-            currPlayer = this.type === 'max' ? 'x' : 'o';
-
-        this.state.split("").forEach((value, index) => {
-            if (value === '0') {
-                let nextState = this.state.split("");
-                nextState[index] = currPlayer;
-                childs.push(new MyNode({
-                    type: this.type === 'min' ? 'max' : 'min',
-                    state: nextState.join(""),
-                    parent: this
-                }))
-            }
-        });
-
-        return childs;
-    }
-}
